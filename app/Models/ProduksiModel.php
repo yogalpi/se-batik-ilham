0d07e1122b0a446ac95021ca510393dffe88096b
said-boy <alkhudrimsaid733@gmail.com>
2024-04-21 13:58:56 +0700

    produksi

--- app/Models/ProduksiModel.php
+++ app/Models/ProduksiModel.php
@@ -8,5 +8,6 @@ class ProduksiModel extends Model
 {
     protected $table      = 'produksi';
     protected $primaryKey = 'kode_produksi';
+
     protected $allowedFields = ['kode_pengadaan' ,'kode_produksi', 'tanggal_mulai', 'tanggal_selesai', 'status'];
 }
