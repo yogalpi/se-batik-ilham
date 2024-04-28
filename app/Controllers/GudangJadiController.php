84c1e03ae3dfdeb766617be77879a31042191440
said-boy <alkhudrimsaid733@gmail.com>
2024-04-28 08:04:43 +0700

    revert-code

--- app/Controllers/GudangJadiController.php
+++ app/Controllers/GudangJadiController.php
@@ -17,35 +17,117 @@ class GudangJadiController extends BaseController
         $this->detail_gudang_jadi = new DetailGudangJadiModel();
     }
 
-    public function dataGudangJadi(){
+    public function dataGudangJadi()
+    {
         $data = [
             'gudang_jadi' => $this->gudang_jadi->findAll(),
         ];
         return view('dataGudangJadi', $data);
     }
-    public function inputGudangJadi(){
+    public function inputGudangJadi()
+    {
         $data = [
             'produksi' => $this->produksi->findAll()
         ];
         return view('inputGudangJadi', $data);
     }
 
-    public function simpanGudangJadi(){
-        $post = $this->request->getPost(['kode_barang', 'kode_produksi', 'nama_barang','ukuran', 'jumlah', 'harga']);
+    public function simpanGudangJadi()
+    {
+        $post = $this->request->getPost(['kode_barang', 'kode_produksi', 'nama_barang', 'ukuran', 'jumlah', 'harga']);
         $this->gudang_jadi->db->transStart();
         $this->gudang_jadi->insert([
             'kode_produksi' => $post["kode_produksi"],
             'kode' => $post["kode_barang"],
             'nama_pakaian' => $post["nama_barang"]
         ]);
-        $this->detail_gudang_jadi->insert([
-            'kode_gudang_jadi' => $post["kode_barang"],
-            'kode' => $post["kode_barang"],
-            'ukuran' => $post["ukuran"],
-            'jumlah' => $post["jumlah"],
-            'harga' => $post["harga"]
-        ]);
+        for ($i = 0; $i < count($post["ukuran"]); $i++) {
+            $this->detail_gudang_jadi->insert([
+                'kode_gudang_jadi' => $post["kode_barang"],
+                'kode' => $post["kode_barang"],
+                'ukuran' => $post["ukuran"][$i],
+                'jumlah' => $post["jumlah"][$i],
+                'harga' => $post["harga"][$i]
+            ]);
+        }
         $this->gudang_jadi->db->transComplete();
+        return redirect()->to("/data_gudang_jadi");
+    }
+
+    public function detailGudangJadi($id)
+    {
+        $data = [
+            'detail_gudang_jadi' => $this->gudang_jadi->join("detail_gudang_jadi", "detail_gudang_jadi.kode_gudang_jadi = gudang_jadi.kode")->where("gudang_jadi.kode", $id)->findAll()
+        ];
+        return view('detailGudangJadi', $data);
+    }
+
+    public function editGudangJadi($id)
+    {
+        $data = [
+            'produksi' => $this->produksi->findAll(),
+            'detail_gudang_jadi' => $this->gudang_jadi->join("detail_gudang_jadi", "detail_gudang_jadi.kode_gudang_jadi = gudang_jadi.kode")->where("gudang_jadi.kode", $id)->findAll()
+        ];
+        return view('editGudangJadi', $data);
+    }
+
+    public function deleteItemGudangJadi($kode, $ukuran)
+    {
+        $this->detail_gudang_jadi->where([
+            'kode' => $kode,
+            'ukuran' => $ukuran
+        ])->delete();
+        return redirect()->to("/edit_gudang_jadi/{$kode}");
+    }
+
+    public function updateGudangJadi()
+    {
+        $post = $this->request->getPost(['kode_barang', 'kode_produksi', 'nama_barang', 'ukuran', 'jumlah', 'harga']);
+        $this->gudang_jadi->db->transStart();
+
+        $this->gudang_jadi->where([
+            'kode_produksi' => $post["kode_produksi"],
+            'kode' => $post["kode_barang"],
+        ])->set([
+            'nama_pakaian' => $post["nama_barang"]
+        ])->update();
+
+        for ($i = 0; $i < count($post["ukuran"]); $i++) {
+            $isExist = $this->detail_gudang_jadi->where([
+                'kode_gudang_jadi' => $post["kode_barang"],
+                'kode' => $post["kode_barang"],
+                'ukuran' => $post["ukuran"][$i],
+            ])->first();
+
+            if ($isExist != null) {
+                $this->detail_gudang_jadi->where([
+                    'kode_gudang_jadi' => $post["kode_barang"],
+                    'kode' => $post["kode_barang"],
+                    'ukuran' => $post["ukuran"][$i],
+                ])->set([
+                    'jumlah' => $post["jumlah"][$i],
+                    'harga' => $post["harga"][$i]
+                ])->update();
+            } else {
+                $this->detail_gudang_jadi->insert([
+                    'kode_gudang_jadi' => $post["kode_barang"],
+                    'kode' => $post["kode_barang"],
+                    'ukuran' => $post["ukuran"][$i],
+                    'jumlah' => $post["jumlah"][$i],
+                    'harga' => $post["harga"][$i]
+                ]);
+            }
+        }
+
+        $this->gudang_jadi->db->transComplete();
+        return redirect()->to("/edit_gudang_jadi/{$post['kode_barang']}");
+    }
+
+    public function deleteGudangJadi($kode)
+    {
+        $this->gudang_jadi->where([
+            'kode' => $kode,
+        ])->delete();
         return redirect()->to("/data_gudang_jadi");
     }
 }
--- app/Views/inputGudangJadi.php
+++ app/Views/inputGudangJadi.php
@@ -17,26 +17,53 @@
               <label for="exampleInputPassword1" class="form-label">Kode Produksi</label>
               <select name="kode_produksi" id="" class="form-control">
                 <option selected disabled>Silahkan Pilih Dari Produksi apa</option>
-              <?php foreach ($produksi as $prod) : ?>
-                <option value="<?= $prod["kode_produksi"]?>">[ <?= $prod["tanggal_mulai"]?> : <?= $prod["tanggal_selesai"]?> ] <?= $prod["kode_produksi"]?></option>
+                <?php foreach ($produksi as $prod) : ?>
+                  <option value="<?= $prod["kode_produksi"] ?>">[ <?= $prod["tanggal_mulai"] ?> ] <?= $prod["kode_produksi"] ?></option>
                 <?php endforeach; ?>
               </select>
             </div>
             <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Nama Barang</label>
-              <input name="nama_barang" type="text" class="form-control" id="exampleInputPassword1">
+              <label for="exampleInputEmail1" class="form-label">Nama Barang</label>
+              <input name="nama_barang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
             </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Ukuran</label>
-              <input name="ukuran" type="text" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">Jumlah</label>
-              <input name="jumlah" type="number" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-5">
-              <label for="exampleInputPassword1" class="form-label">Harga</label>
-              <input name="harga" type="number" class="form-control" id="exampleInputPassword1">
+            <table class="table" id="mytable">
+              <thead>
+                <tr>
+                  <th scope="col">Ukuran</th>
+                  <th scope="col">Jumlah</th>
+                  <th scope="col">Harga</th>
+                  <th scope="col">Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr id="mytr">
+                  <td>
+                    <input name="ukuran[]" type="text" class="form-control" id="exampleInputPassword1">
+                  </td>
+                  <td>
+                    <input name="jumlah[]" type="number" class="form-control" id="exampleInputPassword1">
+                  </td>
+                  <td>
+                    <input name="harga[]" type="number" class="form-control" id="exampleInputPassword1">
+                  </td>
+                  <td>
+                    <span href="#" class="btn btn-danger form-control delete-row" onclick="(function(){
+                      document.querySelectorAll('.delete-row').forEach(function(button) {
+                          button.addEventListener('click', function() {
+                            this.parentNode.parentNode.remove();
+                          });
+                      });
+                    }())">Hapus</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <div class="d-flex justify-content-center mb-5">
+              <span class="rounded-circle" onclick="addRow()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                  <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3z" />
+                </svg>
+              </span>
             </div>
             <button type="submit" class="btn btn-primary">Simpan</button>
           </form>
